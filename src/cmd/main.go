c9a520e45f764a3e4d21ccdd695c05bc53817105
mert-celik-1 <mertcelik2399@gmail.com>
2024-05-06 23:43:04 +0300

    redis

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go-web-api/src/config"
+	"go-web-api/src/infra/cache"
+	"time"
+)
 
 func main() {
 	fmt.Println("!... Hello World ...!")
+
+	cfg := config.GetConfig()
+
+	err := cache.InitRedis(cfg)
+	defer cache.CloseRedis()
+
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	cli := cache.GetRedis()
+
+	cache.Set(cli, "as2", "test2", time.Hour)
+
+	as, _ := cache.Get[string](cli, "as2")
+
+	fmt.Println(as)
+
 }
